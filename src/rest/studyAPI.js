0a59623b4ff0227846f5864fa8bffba70ac7414f
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2023-12-11 18:54:34 +0100

    Inject informations into about dialog (#81)

---
+++ src/rest/studyAPI.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { backendFetchJson } from '../utils/rest-api';
+
+const API_URL =
+    process.env.REACT_APP_API_PREFIX +
+    (process.env.REACT_APP_USE_AUTHENTICATION === 'true'
+        ? process.env.REACT_APP_GATEWAY_PREFIX + '/study/v1'
+        : process.env.REACT_APP_STUDY_URI + '/v1');
+
+export function getServersInfos(token) {
+    return backendFetchJson(
+        `${API_URL}/servers/infos`,
+        {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            cache: 'default',
+        },
+        token
+    );
+}
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -19,4 +19,12 @@ module.exports = function (app) {
             }
         )
     );
+    app.use(
+        createProxyMiddleware(
+            'http://localhost:5001/api/study-server',
+            {
+                pathRewrite: { '^/api/study-server/': '/' },
+            }
+        )
+    );
 };
