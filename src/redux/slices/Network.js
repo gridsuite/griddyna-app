eb50ffa3fcff33c0ecebf6eaf5e3102a45fa058a
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-03-19 14:33:29 +0100

    Apply -Prettier 2.8.7 -> 3.2.5 -eslint-config-prettier 8.8.0 -> 9.1.0 -eslint-plugin-prettier 4.2.1 -> 5.1.3

--- src/redux/slices/Network.js
+++ src/redux/slices/Network.js
@@ -40,11 +40,10 @@ export const getNetworkValues = (propertyValues, equipmentType, fullProperty) =>
         ?.find(
             (propertyValuesItem) => propertyValuesItem.type === equipmentType
         )
-        ?.values[fullProperty?.name]?.map((value) =>
-            fullProperty?.type === PropertyType.BOOLEAN
-                ? value === 'true'
-                : value
-        ) ?? [];
+        ?.values[
+            fullProperty?.name
+        ]?.map((value) => (fullProperty?.type === PropertyType.BOOLEAN ? value === 'true' : value)) ??
+    [];
 
 export const makeGetNetworkValues = () =>
     createSelector(
