3a5c30d242a8a3d43a1e83952e1531ac78ba67cb
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2025-09-18 11:16:20 +0200

    Check styles declaration against MUI `Theme` with TypeScript (#150)

--- src/components/2-molecules/TabBarStyles.ts
+++ src/components/2-molecules/TabBarStyles.ts
@@ -4,10 +4,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+import { type MuiStyles } from '@gridsuite/commons-ui';
 
 export const styles = {
     tabWithError: {
         '&.Mui-selected': { color: 'error.main' },
         color: 'error.main',
     },
-};
+} as const satisfies MuiStyles;
