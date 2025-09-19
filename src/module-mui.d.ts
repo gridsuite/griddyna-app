3e8b5cd846b0669a87e7e4ce155a6a094be4696c
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2025-09-19 19:33:39 +0200

    Fix custom theme variables used

--- src/module-mui.d.ts
+++ src/module-mui.d.ts
@@ -6,34 +6,5 @@
  */
 /* eslint-disable spaced-comment */
 
-// https://mui.com/material-ui/customization/theming/#typescript
-// eslint-disable-next-line import/no-extraneous-dependencies -- lib from MUI
-import { CSSObject } from '@mui/styled-engine';
-import type { Property } from 'csstype';
-// import { Theme as MuiTheme, ThemeOptions as MuiThemeOptions } from '@mui/material/styles/createTheme';
-
-declare module '@mui/material/styles' {
-    type ThemeExtension = {
-        arrow: CSSObject;
-        arrow_hover: CSSObject;
-        circle: CSSObject;
-        circle_hover: CSSObject;
-        link: CSSObject;
-        row: CSSObject;
-        aggrid: {
-            theme: 'ag-theme-alpine' | 'ag-theme-alpine-dark';
-            highlightColor: Property.Color;
-        };
-        agGridBackground: CSSObject;
-        mapboxStyle: string;
-    };
-
-    export interface Theme extends /*MuiTheme,*/ ThemeExtension {}
-
-    // allow configuration using `createTheme`
-    export interface ThemeOptions extends /*MuiThemeOptions,*/ Partial<ThemeExtension> {}
-}
-
-declare module '@mui/utils/capitalize' {
-    export default function capitalize<S extends string>(string: S): Capitalize<S>;
-}
+// Just to be sure that commons-ui's mui augmentation is seen by tsc
+import type {} from '@gridsuite/commons-ui';
