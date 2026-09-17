dc7f1ad683f17f5289e8d113e31f6c452621b1e4
Thang PHAM <phamthang37@gmail.com>
2026-09-17 18:49:28 +0200

    Sonar
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/components/1-atoms/buttons/AttachButton.tsx
+++ src/components/1-atoms/buttons/AttachButton.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -10,9 +10,9 @@ import { Button, SxProps, Tooltip } from '@mui/material';
 type AttachButtonProps = {
     label: string;
     tooltip: string;
-    disabled?: boolean | undefined;
+    disabled?: boolean;
     onClick: () => void;
-    variant?: 'contained' | undefined;
+    variant?: 'contained';
     sx: SxProps;
 };
 
--- src/components/1-atoms/buttons/DetachButton.tsx
+++ src/components/1-atoms/buttons/DetachButton.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -9,10 +9,10 @@ import { Button, SxProps, Tooltip } from '@mui/material';
 
 type DetachButtonProps = {
     label: string;
-    tooltip?: string | undefined;
-    disabled?: boolean | undefined;
+    tooltip?: string;
+    disabled?: boolean;
     onClick: () => void;
-    variant?: 'outlined' | undefined;
+    variant?: 'outlined';
     sx: SxProps;
 };
 
